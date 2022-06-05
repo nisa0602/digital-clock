06c6d72689a5f2ff4c3a08297ad33e937b31eb12
nisa0602 <nisahidayatika999@gmail.com>
2022-06-05 19:03:06 +0700

    styling

--- script.js
+++ script.js
@@ -4,22 +4,10 @@ function setTime(){
     let hrs = getTime.getHours();
     let min = getTime.getMinutes();
     let sec = getTime.getSeconds();
-    // let session = document.get('session');
-
-    // if(hrs >= 12){
-    //     session.innerHTML = 'PM';
-    // }else{
-    //     session.innerHTML = 'AM';
-    // }
-
-    // if(hrs > 12){
-    //     hrs = hrs - 12;
-    // }
 
     document.getElementById('hour').innerHTML = hrs;
     document.getElementById('minute').innerHTML = min;
     document.getElementById('second').innerHTML = sec;
-    document.getElementsByClassName('check').innerHTML='Berhasil'
 }
 
 
